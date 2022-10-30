766a69f126cbcec85e4653c8564c75b1d8f9386c
Simon Lynch <srlynch@gmail.com>
2022-10-30 18:17:48 +1100

    fix vmext

--- variables.tf
+++ variables.tf
@@ -27,7 +27,7 @@ variable "extension_script" {
   type    = string
   default = <<SETTINGS
  {
-  "commandToExecute": ""cd /tmp;git clone https://github.com/hashicorp-demo-lab/demo-static-content.git;cd /var/html/www;sudo cp -r /tmp/demo-static-content/. .;sudo systemctl restart nginx"
+  "commandToExecute": "cd /tmp;git clone https://github.com/hashicorp-demo-lab/demo-static-content.git;cd /var/html/www;sudo cp -r /tmp/demo-static-content/. .;sudo systemctl restart nginx"
  }
 SETTINGS
 
